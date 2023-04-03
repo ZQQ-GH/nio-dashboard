8cdae247506b4299a94a20dd9682e04e48c90207
ZQQ-GH <694246049@qq.com>
2023-04-03 15:24:59 +0800

    cost 300 2

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import Home from '@/pages/Home'
+
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+
+    ]
+})
